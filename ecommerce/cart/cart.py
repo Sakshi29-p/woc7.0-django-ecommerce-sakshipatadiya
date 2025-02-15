fa30741fd2ba77c63a75effe74b0d8d461edfe0a
Sakshi Patadiya <202101469@daiict.ac.in>
2025-02-15 18:56:23 +0530

    added functionality to update the cart and functionality related to quanity

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -12,16 +12,26 @@ class Cart():
         self.cart = cart
 
     
-    def add(self,product):
+    def add(self,product,quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id]={'price': str(product.price)}
+            self.cart[product_id]=int(product_qty)
 
         self.session.modified = True
 
+    
+    def update(self,product,quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+        self.cart[product_id]=product_qty
+
+        self.session.modified = True
+
+        return self.cart
     
     def __len__(self):
         return len(self.cart)
@@ -31,4 +41,8 @@ class Cart():
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
         return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
 
